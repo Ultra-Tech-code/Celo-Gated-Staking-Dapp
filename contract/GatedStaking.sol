a9e1a924d5e17ca9a411dc9c37c2e0eef3490577
Ultra-tech-code <hizick27@gmail.com>
2022-11-08 21:54:02 +0100

    first commit

---
+++ contract/GatedStaking.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.9;
+
+contract GatedStaking{
+
+    uint constant rate = 3854;
+    uint256 constant factor = 1e11;
+    address owner;
+
+    address constant cUSDAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+
+    struct stakeInfo{
+        address staker;
+        address tokenStaked;
+        uint amountStaked;
+        uint timeStaked;
+    }
+
+    mapping(address => mapping(address => stakeInfo)) public usersStake;
+
+    function stake (address _tokenAddress, uint _amount) public {
+        require(IERC20(cUSDAddress).balanceOf(msg.sender) > 3, "User does not have a Celo Token balance that is more than 3");
+        require(IERC20(_tokenAddress).balanceOf(msg.sender) > _amount, "insufficient balance");
+        IERC20(_tokenAddress).transferFrom(msg.sender, address(this), _amount);
+        stakeInfo storage ST = usersStake[msg.sender][_tokenAddress];
+        if(ST.amountStaked > 0){
+            uint interest = _interestGotten(_tokenAddress);
+            ST.amountStaked += interest;
+        }
+        ST.staker = msg.sender;
+        ST.amountStaked = _amount;
+        ST.tokenStaked = _tokenAddress;
+        ST.timeStaked = block.timestamp;
+    }
+
+
+    function withdraw(address _tokenAddress, uint _amount) public{
+        stakeInfo storage ST = usersStake[msg.sender][_tokenAddress];
+        require(ST.amountStaked > _amount, "insufficient balance");
+        uint interest = _interestGotten(_tokenAddress);
+        ST.amountStaked -= _amount;
+        uint totalAmount = _amount + interest;
+        IERC20(_tokenAddress).transferFrom(address(this), msg.sender, totalAmount);
+
+    }
+
+
+    function _interestGotten(address _tokenAddress) internal view returns(uint ){
+        stakeInfo storage ST = usersStake[msg.sender][_tokenAddress];
+        uint interest;
+        if(ST.amountStaked > 0){
+            uint time = ST.timeStaked - block.timestamp;
+            uint principal = ST.amountStaked;
+            interest = principal * rate + time;
+             interest /=  factor;
+        }
+        return interest;
+    }
+
+}
+
+interface IERC20{
+    function transfer(address to, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+
+}
